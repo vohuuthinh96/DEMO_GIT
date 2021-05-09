28e922e0f5760f8faf7dc7d9842009bdef22bad2
thinhvh <thinhvohuu@git.sohacorp.vn>
2021-05-09 12:16:46 +0700

    reset

--- app/src/main/java/com/example/demogit/MainActivity.kt
+++ app/src/main/java/com/example/demogit/MainActivity.kt
@@ -7,7 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // sua lan 1 o demo 2
-        // sua lan 2 o demo 2
+        //reset
     }
 }
