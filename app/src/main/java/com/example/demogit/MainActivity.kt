fdebac732e718aeb9ff68dc9306d00389305cc1a
thinhvh <thinhvohuu@git.sohacorp.vn>
2021-05-09 11:50:35 +0700

    reset git

--- app/src/main/java/com/example/demogit/MainActivity.kt
+++ app/src/main/java/com/example/demogit/MainActivity.kt
@@ -7,9 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // sua o master lan 1
-        // sua o demo lan1
-        // sua o master lan 2
-        // sua o demo lan 2
+
     }
 }
