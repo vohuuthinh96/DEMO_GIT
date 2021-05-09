c59fb1efe07f9dff005f1f49e38d67513029aeef
thinhvh <thinhvohuu@git.sohacorp.vn>
2021-05-09 11:51:47 +0700

    sua lan 1 o demo 2

--- app/src/main/java/com/example/demogit/MainActivity.kt
+++ app/src/main/java/com/example/demogit/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        // sua lan 1 o demo 2
     }
 }
